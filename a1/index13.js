2b7be5dacfcb7dbb8bc454254caec5c6b35555bd
Abdulaziz-06 <aaziz871048@gmail.com>
2025-05-16 19:40:39 +0530

    updated

---
+++ a1/index13.js
@@ -0,0 +1,11 @@
+const prod = [
+    { id: 1, name: "prod1", price: 1000 },
+    { id: 2, name: "prod2", price: 2000 },
+    { id: 3, name: "prod3", price: 3000 }
+];
+
+const element = prod.map((element) => {
+    element.price += 5;
+    console.log(element.name + " " + element.price);
+    return element; 
+});
