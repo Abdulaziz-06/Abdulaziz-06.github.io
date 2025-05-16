ce9cc1bb900ec52e863abeb894f25ddc9a322c70
Abdulaziz-06 <aaziz871048@gmail.com>
2025-05-16 19:30:25 +0530

    updated

---
+++ a1/index12.js
@@ -0,0 +1,6 @@
+const prod=[
+    {id:1,"name":"prod1",price:1000},
+    {id:2,"name":"prod2",price:2000},
+    {id:3,"name":"prod3",price:3000}
+]
+prod.forEach((element)=>console.log(element.name));
